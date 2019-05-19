2435b8b87262e3296d8f029e2b560d54c76e652d
Braedon Giblin <braedondgilin@gmail.com>
2019-05-18 22:17:23 -0500

    fixed compile errors

--- arrayListTest.c
+++ arrayListTest.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include "intArrayList.h"
-#include "intArrayList.c"
 
 int main() {
   intArrayList* arrLi = intArrayList_create(10);
-  intArrayList* source = intArrayList_create(5);
+  //intArrayList* source = intArrayList_create(5);
 
-  intArrayList_add(arrLi, 10);
-  intArrayList_set(arrLi, 9, 0);
+  intArrayList_add(arrLi, 1);
+  intArrayList_add(arrLi, 2);
+  intArrayList_add(arrLi, 3);
+  intArrayList_add(arrLi, 4);
+  intArrayList_add(arrLi, 5);
 
   for (int i = 0; i < intArrayList_size(arrLi); i++) {
     printf("%3d\n", intArrayList_get(arrLi, i));
   }
 
-  
-  intArrayList_clear(&source);
+
+  //intArrayList_clear(&source);
   intArrayList_clear(&arrLi);
 }
--- intArrayList.c
+++ intArrayList.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "intArrayList.h"
 
 intArrayList* intArrayList_create(int length) {
